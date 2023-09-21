c76f686afeb091ae32bbcb648a9e9d5e4332053b
quiet-pirate <ale.alducin@gmail.com>
2023-09-21 11:07:16 -0600

    .

---
+++ chamber/scripts/getDate.js
@@ -0,0 +1,6 @@
+const currentYear = 2023;
+document.querySelector('#year').textContent = currentYear;
+
+let text = document.lastModified;
+const date = new Date(document.lastModified);
+document.getElementById('currentDate').innerHTML = text;
