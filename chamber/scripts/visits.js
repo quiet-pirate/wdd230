17a96457be9970f04980a195faff56c9ba7ad0a4
quiet-pirate <ale.alducin@gmail.com>
2023-09-30 21:41:13 -0600

    updated discover

---
+++ chamber/scripts/visits.js
@@ -0,0 +1,43 @@
+//select element
+const visitsDisplay = document.querySelector(".visits");
+//define todays date
+const currentDate = Date.now();
+//set num of visits counter
+//if first time: display Welcome message
+//if visit less than a day display message2
+//else: message3 days betwwen visits
+    //if message 3 = 1 change to day
+
+//--------------CodePen Example---------------//
+
+let numVisits = Number(window.localStorage.getItem("numVisits-Is"))|| 0;
+if (numVisits !==0){
+    visitsDisplay.textContent = numVisits;
+    //If the amount of time between visits is less than a day, display "Back so soon! Awesome!".
+
+} else{
+    visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`
+}
+numVisits++;
+localStorage.setItem("numVisits-Is", numVisits);
+
+// initialize display elements
+const todayElement = document.querySelector("#today");
+const christmasElement = document.querySelector("#christmas");
+const christmasDateElement = document.querySelector("#christmasDate");
+const daysElement = document.querySelector("#daysleft");
+
+// processing
+const today = Date.now();
+const christmasDate = new Date(Date.UTC(theDateToday.getFullYear(), 11, 25));
+// check if is the waing days of December, if so ... change to next year.
+if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
+	christmasDate.setFullYear(christmasDate.getFullYear() + 1);
+}
+// find difference between epoch times in ms and convert to days
+let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
+
+todayElement.textContent = today;
+christmasElement.textContent = christmasDate.getTime();
+christmasDateElement.textContent = christmasDate;
+daysElement.textContent = `${daysleft.toFixed(0)} days`;
