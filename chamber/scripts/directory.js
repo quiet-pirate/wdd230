b922a3a211de136c4178822779b6b92682b4e753
quiet-pirate <ale.alducin@gmail.com>
2023-10-11 23:47:31 -0600

    .

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,57 @@
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
+const url='https://quiet-pirate.github.io/wdd230/chamber/data/members.json';
+
+async function getMembers(){
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+async function displayMembers(members){
+    const cards = document.querySelector('#cards');
+
+    members.forEach((member)=>{
+        let card = document.createElement("section");
+        let h2 = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let url = document.createElement('a');
+        let image = document.createElement('img');
+
+        h2.textContent = `${member.name}`;
+        address.textContent = `${member.address}`;
+        phone.textContent = `${member.phoneNumber}`;
+        url.textContent = `${member.url}`;
+
+        image.setAttribute('src', member.image);
+
+
+        card.appendChild(h2);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+        card.appendChild(image);
+
+        cards.appendChild(card);
+    });
+}
+getMembers();
+
+
--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,32 +1,35 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-const cards = document.querySelector('#cards');
 
 async function getProphetData(){
     const response = await fetch(url);
     const data = await response.json();
-    displayProphets(data.prophets);    
-}
+    displayProphets(data.prophets);
+ }
 
-getProphetData();
+async function displayProphets(prophets){
+    const cards = document.querySelector('#cards');
 
-const displayProphets = (prophets) => {
-    prophets.forEach((prophet)=>{
+    prophets.forEach((prophet) => {
         let card = document.createElement('section');
-        let fullName = document.createElement('h2');
+        let h2 = document.createElement('h2');
         let portrait = document.createElement('img');
 
         h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt',`Portrait of ${prophet.name} ${prophet.lastname}`);
-        portrait.setAttribute('loading','lazy');
-        portrait.setAttribute('width','340');
-        portrait.setAttribute('height','440');
-    
-        card.appendChild(fullName);
-        card.aapendChild(portrait);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ______________`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(h2);
+        card.appendChild(portrait);
 
         cards.appendChild(card);
-    });
-}
+  }); 
+} 
+
+   
+ getProphetData();
 
 
