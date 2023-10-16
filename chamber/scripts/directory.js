ebf92d1a5468ee76dabdf87f58b729e7f62aa233
quiet-pirate <ale.alducin@gmail.com>
2023-10-16 12:09:14 -0600

    .

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,22 +1,3 @@
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}
-
 const url='https://quiet-pirate.github.io/wdd230/chamber/data/members.json';
 
 async function getMembers(){
@@ -57,4 +38,21 @@ async function displayMembers(members){
 }
 getMembers();
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
 
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
